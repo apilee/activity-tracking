4fd95fd694e8adc76465c9393fa2bbefcbc9ec9f
Bartek Kalinka <bartkali@hotmail.com>
2016-07-11 17:35:26 +0200

    spark prints to file

--- sparkApp/src/main/scala/activity/Main.scala
+++ sparkApp/src/main/scala/activity/Main.scala
@@ -1,19 +1,29 @@
 package activity
 
+import java.io.{File, PrintWriter}
+
 import org.apache.spark.{SparkConf, SparkContext}
 import com.datastax.spark.connector._
 
 object Main {
+  val writer = new PrintWriter(new File("output.txt"))
+
   def run(cassandraHostIP: String) = {
     val conf = new SparkConf(true).set("spark.cassandra.connection.host", cassandraHostIP)
     val sc = new SparkContext("local[*]", "spark-activity-tracking", conf)
     val rdd = sc.cassandraTable("activitytracking", "trainingorientation")
-    println("111111111111111111111111")
-    println(s"rdd.count ${rdd.count()}")
-    println(s"rdd.first ${rdd.first()}")
-    println(s"yaw sum " + rdd.map(_.getInt("yaw")).sum)
+    writer.println("111111111111111111111111")
+    writer.println(s"rdd.count ${rdd.count()}")
+    writer.println(s"rdd.first ${rdd.first()}")
+    writer.println(s"yaw sum " + rdd.map(_.getInt("yaw")).sum)
   }
 
-  def main(args: Array[String]): Unit = run(args(0))
+  def main(args: Array[String]): Unit = {
+    try {
+      run(args(0))
+    } finally {
+      writer.close()
+    }
+  }
 }
 
